e99cff05e0b0c69625ede35bed72127751988990
priyansh <priyanshkanoje254@gmail.com>
2023-04-15 20:52:33 +0530

    Data type Stroages questions and type casting or conversion questions

---
+++ src/fundamentals/DataTypeStroages.java
@@ -0,0 +1,16 @@
+package fundamentals;
+
+public class DataTypeStroages {
+    public static void main(String[] args) {
+
+//        int min = Integer.MIN_VALUE;
+//        int max = Integer.MAX_VALUE;
+//
+//        System.out.print(min + " " + max);
+
+        char ch = 'a';
+        char c = 'b';
+        System.out.print('a' + 3 + 'b');
+
+    }
+}
---
+++ src/fundamentals/TypeCasting.java
@@ -0,0 +1,19 @@
+package fundamentals;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+//         char ch = 'a';
+//         int i = ch;
+//         System.out.print(i);
+//
+//        float f = (float)1.7;
+//        int i = (int)10.6;
+
+        System.out.println(4+4);
+        System.out.println(4+4.5);
+        System.out.println(4.1+4.4);
+        System.out.println(2.0+4);
+
+    }
+
+}
