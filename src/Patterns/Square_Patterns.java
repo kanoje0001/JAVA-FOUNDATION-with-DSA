6e5b8c08a36b3f14db1be67cd148259a4ab16021
priyansh <priyanshkanoje254@gmail.com>
2023-04-18 13:11:46 +0530

    Square patterns

--- src/Patterns/Square_Patterns.java
+++ src/Patterns/Square_Patterns.java
@@ -1,3 +1,4 @@
+//Question on square pattern in Number form
 package Patterns;
 
 import java.util.Scanner;
@@ -6,11 +7,18 @@ public class Square_Patterns {
 
     public static void main(String[] args) {
         int n;
+        System.out.print("Enter the Number :- ");
         Scanner s = new Scanner(System.in);
         n = s.nextInt();
         int i = 1;
         while(i<=n){
-
+         int j = 1;
+         while(j <= n){
+             System.out.print(j);
+             j = j+1;
+         }
+         System.out.println();
+         i = i+1;
         }
     }
 
