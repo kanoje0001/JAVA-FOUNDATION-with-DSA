b65b4c31fcc2dfe67873518521d5c69ea09ee15c
priyansh <priyanshkanoje254@gmail.com>
2023-06-02 14:25:38 +0530

    ceiling question

---
+++ src/InterviewQuestions/floor.java
@@ -0,0 +1,31 @@
+package InterviewQuestions;
+
+public class floor {
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,9,14,16,18};
+        int target = 15;
+        int ans = floor(arr,target);
+        System.out.println("the index in arrys is" + ans);
+    }
+
+    //returnning rhe index of smallest number
+    public static int floor(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                end = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return end;
+
+    }
+}
